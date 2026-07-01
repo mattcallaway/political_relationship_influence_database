293ffd0640fb37607e98f19d34bdc3020d1b0f1b
Matt Callaway <mattcallaway@github.com>
2026-07-01 00:57:40 -0700

    Implement source list template and details view

--- apps/sources/views.py
+++ apps/sources/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from apps.sources.models import Source
 
 def source_list(request):
-    return render(request, 'sources/source_list.html', {})
+    sources = Source.objects.all().order_by('-publication_date')
+    return render(request, 'sources/source_list.html', {
+        'sources': sources
+    })
